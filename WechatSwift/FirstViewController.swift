c8dcef048dcb19e4b1c947c5f91076892bab94a3
GeforceLee <314004856@qq.com>
2016-07-26 15:51:51 +0800

    init

---
+++ WechatSwift/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  WechatSwift
+//
+//  Created by Yunlong Li on 16/7/26.
+//  Copyright © 2016年 GlobalTravel. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
